3f0c5b10b2e87fe6050c142393d471e96c177d36
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-05-17 14:00:58 +0530

    server/sabertooth: Integrate with dearpygui, async
    Removed Tk/Tkinter parts, stripped down to just the bare connection
    functions, the gui part will be handled by gui/motor.
    
    Modified zmq calls to use async and made private methods public with a
    Command-pattern based launcher to follow in gui/motor.

--- server/sabertooth.py
+++ server/sabertooth.py
@@ -4,7 +4,8 @@
 
 # Importing zmq for client-server connection and tkinter for gui and keybindings
 import zmq
-import tkinter as tk
+import zmq.asyncio
+import asyncio
 
 
 class Sabertooth:
@@ -20,9 +21,9 @@ class Sabertooth:
         self.context = context
 
         # Starting a new tkinter window and setting the title and size
-        self.window = tk.Tk()
-        self.window.geometry("400x350")
-        self.window.title("Sabertooth")
+        # self.window = tk.Tk()
+        # self.window.geometry("400x350")
+        # self.window.title("Sabertooth")
 
     def _connect(
         self,
@@ -34,39 +35,34 @@ class Sabertooth:
 
     def start(self):
 
-        self.label = tk.Label(self.window, text="Empty")
-        self.label.grid(column=2, row=3)
+        # self.label = tk.Label(self.window, text="Empty")
+        # self.label.grid(column=2, row=3)
 
         self._connect()
 
-        self.window.bind("w", self._forward)
-        self.window.bind("s", self._backward)
-        self.window.bind("a", self._left)
-        self.window.bind("d", self._right)
-        self.window.bind("x", self._stop)
+        # self.window.bind("w", self.forward)
+        # self.window.bind("s", self.backward)
+        # self.window.bind("a", self.left)
+        # self.window.bind("d", self.right)
+        # self.window.bind("x", self.stop)
 
-        self.window.mainloop()
+        # self.window.mainloop()
 
     # Private methods for movement. Here strings are sent to the client in the form of binary
-    def _forward(self, x):
-        self.str_socket.send(b"w")
-        self.label.configure(text="Moving Forward")
+    async def forward(self):
+        return await self.str_socket.send(b"w")
 
-    def _backward(self, x):
-        self.str_socket.send(b"s")
-        self.label.configure(text="Moving backward")
+    async def backward(self):
+        return await self.str_socket.send(b"s")
 
-    def _left(self, x):
-        self.str_socket.send(b"a")
-        self.label.configure(text="Turning left")
+    async def left(self):
+        return await self.str_socket.send(b"a")
 
-    def _right(self, x):
-        self.str_socket.send(b"d")
-        self.label.configure(text="Turning right")
+    async def right(self):
+        return await self.str_socket.send(b"d")
 
-    def _stop(self, x):
-        self.str_socket.send(b"x")
-        self.label.configure(text="Stop")
+    async def stop(self):
+        return await self.str_socket.send(b"x")
 
 
 # for Testing purpose
